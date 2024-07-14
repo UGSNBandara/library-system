04efc017088c2f144318e792c68dbcb9e245e85c
UGSNBandara <sulithanb119@gmail.com>
2024-07-15 02:12:31 +0530

    Add a class to main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+class userC{
+    private:
+        string name;
+        string nic;
+        string adr;
+    public:
+        userC();
+        ~userC();
+
+    private:
+};
+
+
 int main(){
     cout<<"Hello";
     return 0;
