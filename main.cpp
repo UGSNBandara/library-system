5450e7d522c6d886f0dc21de4f40a0141ba4f189
UGSNBandara <sulithanb119@gmail.com>
2024-07-15 02:26:41 +0530

    added few methods to userC class

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+//this class for handle all users basic details
+//3 types of user in this system 1) admin users; 2) Registedred users 3) unregistered users
+
 class userC{
     private:
         string name;
@@ -9,13 +12,51 @@ class userC{
         string adr;
     public:
         userC();
+        userC(string n, string n2, string a); //overloaded constructor
+        userC(userC &arg); //copy constructor 
         ~userC();
-
+        void modify(string n, string n2, string a); //to modyfy after created
+                
     private:
 };
 
+//definition for the methods of userC class
+userC::userC(){
+    name ="";
+    nic = "";
+    adr = "";
+}
+
+userC::~userC(){
+    //this is desructor 
+}
+
+userC::userC(string n, string n2, string a){
+    name = n;
+    nic = n2;
+    adr = a;
+}
+
+userC::userC(userC &arg){
+    name = arg.name;
+    nic = arg.nic;
+    adr = arg.adr;
+}
+
+void userC::modify(string n, string n2, string a){
+    name = n;
+    nic = n2;
+    adr = a;
+}
+
+
+
+
+//start of the main function
 
 int main(){
-    cout<<"Hello";
+    userC user("sulitha", "20030119", "chnabay");
+    cout<<"hello";
+
     return 0;
 }
