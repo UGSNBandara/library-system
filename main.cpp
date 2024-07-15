69a70bbf7fb715355d30258fd6731c56028b874d
UGSNBandara <sulithanb119@gmail.com>
2024-07-15 18:12:45 +0530

    added adminuser class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 //this class for handle all users basic details
@@ -8,7 +8,6 @@ using namespace std;
 class userC{
     private:
         string name;
-        string nic;
         string adr;
     public:
         userC();
@@ -16,8 +15,9 @@ class userC{
         userC(userC &arg); //copy constructor 
         ~userC();
         void modify(string n, string n2, string a); //to modyfy after created
-                
-    private:
+        void printd();
+    protected:
+        string nic;
 };
 
 //definition for the methods of userC class
@@ -49,14 +49,111 @@ void userC::modify(string n, string n2, string a){
     adr = a;
 }
 
+void userC::printd(){
+    cout<<"Name : "<<name<<endl<<"NIC : "<<nic<<endl<<"Address : "<<adr<<endl;
+}
 
+//Admin user Class :: inharited from userC class
+
+class admuser : public userC {
+private:
+    string lib_id;
+    int lv;
+    //super Admin : 1
+    //Library manager : 2
+    //Librarian : 3
+    void idgenarate();
+    void printallLevel();
+    void printlevel();
+
+public:
+    admuser();
+    ~admuser();
+    admuser(string name, string nic, string addr, int level);
+    admuser(admuser &arg);
+    void createAcc();
+    void printcard();   
+};
+
+admuser::admuser(){
+    lib_id = "";
+    lv = 0;
+}
+
+admuser::~admuser(){
+    //destructor 
+}
+
+admuser::admuser(string name, string nic, string addr, int level):userC(name,nic,addr){
+    idgenarate();
+    lv = level;
+}
+
+admuser::admuser(admuser &arg):userC(arg)
+{
+    lv = arg.lv;
+    lib_id = arg.lib_id;
+}
+
+void admuser::idgenarate(){
+    lib_id = "A"+to_string(lv)+nic;
+}
+
+void admuser::createAcc(){
+    string name;
+    string nic;
+    string addr;
+    cout<<"\tWelcom to Library system"<<"Please enter your details to sing in to the system as an Admin User : ";
+    cout<<endl<<"Name : ";
+    cin.ignore();
+    getline(cin, name);
+
+    cout<<"NIC : ";
+    getline(cin, nic);
+
+    cout<<"Address : ";
+    getline(cin, addr);
+
+    cout<<"Select the Level :";
+    printallLevel();
+    cout<<" : ";
+    cin>>lv;
+    
+    modify(name,nic,addr);
+    idgenarate();
+}
+
+void admuser::printallLevel(){
+    cout<<endl<<"1 : Super Admin"<<endl<<"2 : Library Manager"<<endl<<"3 : Librarian"<<endl;
+}
+
+void admuser::printcard(){
+    printd();
+    cout<<"Accout Type : "<<"Admin user"<<endl<<"Level : ";
+    printlevel();
+    cout<<endl<<"User ID : "<<lib_id<<endl;
+}
+
+void admuser::printlevel(){
+    if(lv == 1){
+        cout<<"Super Admin";}
+    else if(lv == 2){
+        cout<<"Library Manager";
+    }
+    else{
+        cout<<"Librarian";
+    }
+
+}
 
 
 //start of the main function
 
 int main(){
     userC user("sulitha", "20030119", "chnabay");
-    cout<<"hello";
+    admuser u;
+    u.createAcc();
+    u.printcard();
 
     return 0;
 }
